2269f1cd128d8661559ab7419da3898dc9c1382b
danilojduarte <76825919+danilojduarte@users.noreply.github.com>
2024-03-05 14:00:57 -0300

    Criando arquivos do projeto

---
+++ cook/src/app/index/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function Index() {
+    return (
+        <View>
+            <Text>Iniciando Com React Native na Prática</Text>
+        </View>
+    )
+}
